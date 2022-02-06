fbe36d17cc25b699fc00cd120d07a74624b3409e
Kartikay Singh <kartikays@Kartikays-MacBook-Pro.local>
2022-02-06 20:34:25 +0530

    BST

---
+++ src/com/algoexpert/problems/Trees/BST.java
@@ -0,0 +1,12 @@
+package com.algoexpert.problems.Trees;
+
+public class BST {
+    int value;
+    BST left;
+    BST right;
+
+    public BST(int value) {
+        this.value = value;
+    }
+    // todo
+}
